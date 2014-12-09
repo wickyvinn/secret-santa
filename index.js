8fc83019475a9b75a0c30a05f4558228b4695f95
wickyvinn <vicky.winn@gmail.com>
2014-12-08 16:25:12 -0800

    Added route to views/index.html

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 var express = require('express')
 var app = express()
 
+// Set port, heroku style
+// Run locally: foreman start web
 app.set('port', (process.env.PORT || 5000)); 
+
+// express.static middleware
 app.use(express.static(__dirname + '/public'));
 
+// ejs will provide a renderFile method w this signature: (path, options, callback)
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'ejs');
+
 app.get('/', function(request, response) {
-  response.send('Hello world!');
+  response.render('index.html');
 });
 
 app.listen(app.get('port'), function() {
